3e57664ab82f362d488663711772e12b165b6155
DarkSim <mansimeon01@gmail.com>
2019-07-26 08:17:54 +0800

    revert

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'admissions';
+    protected $redirectTo = '/';
 
     protected $username = 'uname';
 
