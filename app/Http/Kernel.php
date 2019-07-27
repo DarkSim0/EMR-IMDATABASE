64343b863e4c676c7b9e5ebd6e468fddd97b5624
DarkSim <mansimeon01@gmail.com>
2019-07-27 16:03:43 +0800

    add changes for ie edge

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            //\App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\IeFix::class,
         ],
 
@@ -50,6 +50,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'iefix' => \App\Http\Middleware\IeFix::class
+        'csrf' => 'App\Http\Middleware\VerifyCsrfToken',
+        'iefix' => \App\Http\Middleware\IeFix::class,
     ];
 }
