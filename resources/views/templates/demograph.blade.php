5e404bb3533e8bed0f67c3112241888574823e86
DarkSim <mansimeon01@gmail.com>
2019-08-13 16:12:51 +0800

    updated demographics

--- resources/views/templates/demograph.blade.php
+++ resources/views/templates/demograph.blade.php
@@ -3,12 +3,12 @@
 			<div class="row">
 				<div class="col-md-2">
 					Hospital No.
-				<h1><span class="text-success" >{{$patient->Hospnum}}</span></h1>
+					<h1><span class="text-success" >{{$patient->Hospnum}}</span></h1>
 				</div>
 				<div class="col-md-8">
 
 					<h3 class="" >{{ $patient->lname.', '.$patient->fname.' '.$patient->mname }}</h3>
-					<h4>Gender: @if($patient->sex == 'Male') <span class="text-primary" >{{ $patient->sex }}</span> @else <span style="color:#ff66d9;">{{ $patient->sex }}</span> @endif &nbsp; Age: {{$patient->age}} &nbsp; Civil Status: {{ $patient->cs }} &nbsp; Religion: {{$patient->religion}} </h4>
+					<h4> <span >Gender:</span>	 @if($patient->sex == 'Male') <span class="text-primary" >{{ $patient->sex }}</span> @else <span style="color:#ff66d9;">{{ $patient->sex }}</span> @endif &nbsp; Age: {{$patient->age}} &nbsp; Civil Status: {{ $patient->cs }} &nbsp; Religion: {{$patient->religion}} </h4>
 					<u><h4></h4></u>
 				</div>
 			</div>
